97b712c0be49763fa7e098dd7d73ffa423bdc7d4
Leo Zeng <qingchun1995@gmail.com>
2019-05-05 21:45:42 +0800

    Create a Notification Message Component

--- App.vue
+++ App.vue
@@ -5,6 +5,18 @@
     </div>
 
     <Notification></Notification>
+
+    <Notification notifHeader="Changes in Service">
+      <p>We just updated our privacy policy here to better service our customers. We recommend reviewing the changes.</p>
+    </Notification>
+
+    <Notification notifType="success">
+      <p>You may now log-in with the username you have chosen</p>
+    </Notification>
+
+    <Notification notifType="negative" notifHeader="We're sorry we can't apply that discount">
+      <p>That offer has expired</p>
+    </Notification>
   </div>
 </template>
 
--- components/Notification.vue
+++ components/Notification.vue
@@ -1,10 +1,60 @@
 <template>
-  <div></div>
+  <transition name="fade">
+    <div class="message-container" v-if="show">
+      <div class="ui message" :class="notifType">
+        <i class="close icon" @click="hide"></i>
+        <div class="header">{{ notifHeader }}</div>
+        <slot></slot>
+      </div>
+    </div>
+  </transition>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    notifType: {
+      type: String,
+      default: "info",
+      validator: value =>
+        ["warning", "info", "positive", "success", "negative", "error"].indexOf(
+          value
+        ) !== -1
+    },
+
+    notifHeader: {
+      type: String,
+      default: "Information"
+    }
+  },
+
+  data() {
+    return {
+      show: true
+    };
+  },
+
+  methods: {
+    hide() {
+      this.show = !this.show;
+    }
+  }
+};
 </script>
 
-<style lang="scss" scoped>
+<style >
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.message-container {
+  margin: 20px 0;
+}
 </style>
+
